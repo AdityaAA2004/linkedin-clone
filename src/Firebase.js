4690aa4ff447f6931b20ed322a767b414610f59c
AdityaAA2004 <anandad1@msu.edu>
2025-11-03 17:46:18 -0500

    Add firebase changes

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,19 +2,19 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection } from 'firebase/firestore';
 import { getAuth,setPersistence, browserSessionPersistence } from "firebase/auth";
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId
-  };
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+};
 
-  const firebaseApp = initializeApp(firebaseConfig);
-  const FullDataBase = getFirestore(firebaseApp);
-  const PostsCollection = collection(FullDataBase, 'posts'); // Assuming 'posts' is your Firestore collection name
-  const DescriptionCollection = collection(FullDataBase, 'description'); // Assuming 'posts' is your Firestore collection name
-  const auth = getAuth(firebaseApp);
-  setPersistence(auth, browserSessionPersistence);
+const firebaseApp = initializeApp(firebaseConfig);
+const FullDataBase = getFirestore(firebaseApp);
+const PostsCollection = collection(FullDataBase, 'posts'); // Assuming 'posts' is your Firestore collection name
+const DescriptionCollection = collection(FullDataBase, 'description'); // Assuming 'posts' is your Firestore collection name
+const auth = getAuth(firebaseApp);
+setPersistence(auth, browserSessionPersistence);
 
-  export {PostsCollection,auth,DescriptionCollection};
+export {PostsCollection,auth,DescriptionCollection};
